7ac26292e6b101a7924365243675b9a4c53a58d6
Juhee Dhone <juheedhone12@gmail.com>
2024-12-22 18:40:13 +0530

    init

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  const items = ["new york", "nagpur", "pune", "mumbai"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
---
+++ src/message.tsx
@@ -0,0 +1,12 @@
+// PascalCasing
+
+function Message() {
+  // JSX: javascript XML
+  const name = "cloud hosting for Pros";
+  if (name) {
+    return <h1>{name}</h1>;
+  }
+  return <h1>hello world</h1>;
+}
+
+export default Message;
