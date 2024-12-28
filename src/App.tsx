126693456094af18c2de88ea9f0f2675859096f7
Juhee Dhone <juheedhone12@gmail.com>
2024-12-29 00:35:41 +0530

    Updating Array of Objects

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,26 @@ import { useState } from "react";
 // import Button from "./components/button/button";
 
 function App() {
-  const [tags, setTags] = useState(["happy", "cheerful", "hot"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "bug1", fixed: false },
+    { id: 2, title: "bug2", fixed: false },
+  ]);
 
-  // const handleClick = () => {
-  //   // Add
-  //   setTags([...tags, "exciting"]);
-
-  //   // Remove
-  //   setTags(tags.filter((tag) => tag !== "happy"));
-
-  //   // Update
-  //   setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
-  // };
-
-  const handleAddTag = () => {
-    setTags([...tags, "exciting"]);
-  };
-  const handleRemoveTag = () => {
-    setTags(tags.filter((tag) => tag !== "hot"));
-  };
-  const handleUpdateTag = () => {
-    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+  const handleClick = () => {
+    setBugs(bugs.map((bug) => (bug.id === 2 ? { ...bug, fixed: true } : bug)));
   };
 
   return (
     <>
-      <button onClick={handleAddTag}>Add</button>
-      <button onClick={handleRemoveTag}>Remove</button>
-      <button onClick={handleUpdateTag}>Update</button>
-      <div>{tags.map((tag) => tag + ",")}</div>
+      <button onClick={handleClick}>Fix</button>
+      <div>
+        {bugs.map((bug) => (
+          <div key={bug.id}>
+            <p>{`name: ${bug.title}`}</p>
+            <p>{`status: ${bug.fixed}`}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
