bf6787f0e76d1254d85d5a5f83fe9bbba72eb4cf
Juhee Dhone <juheedhone12@gmail.com>
2024-12-28 16:41:09 +0530

    Choosing the state structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import Like from "./components/Like";
+import { useState } from "react";
+
 
 function App() {
+//   type1
+  // const [firstName, setFirstName]= useState(' ');
+  // const [lastName, setLastName]= useState(' ');
+
+  // const fullName = firstName + ' ' + lastName;
+
+// type 2- Group Related variables inside an object  
+
+  const [person, setPerson] = useState({
+    firstName:'shrikant',
+    lastName:'kalar',
+    // avoid deeply nested structure
+    contact:{
+      address:{
+        street:''
+      }
+    }
+  });
+
+  // const [isLoading, setLoading]= useState(false);
+
+  
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />
+     {person.firstName + ' ' +  person.lastName}
     </div>
   );
 }
