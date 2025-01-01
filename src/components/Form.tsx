fa61864b2347a0074932148770261ddaf108d67a
Juhee Dhone <juheedhone12@gmail.com>
2025-01-01 17:19:37 +0530

    Handling Form Submit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,12 @@
+import type { FormEvent } from "react";
+
 const Form = () => {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("submitted");
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
@@ -8,10 +14,14 @@ const Form = () => {
         <input id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
-        <label htmlFor="Age" className="form-label">Age</label>
+        <label htmlFor="Age" className="form-label">
+          Age
+        </label>
         <input id="Age" type="number" className="form-control" />
       </div>
-      <button className="btn btn-primary" type="submit">Submit</button>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
