89c83278752bb29760c7eca59e7244a468d3b351
Juhee Dhone <juheedhone12@gmail.com>
2025-01-01 17:00:23 +0530

    Building Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import { ExpandableText } from "./components/ExpandableText";
+import Form from "./components/form";
 
-function App() {
-  
+const App = () => {
   return (
-   
-      <ExpandableText >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-        voluptatibus repudiandae laboriosam quae in maiores voluptates
-        reprehenderit? Deserunt quos ratione, voluptas quod iusto illo commodi
-        delectus ipsum reprehenderit aliquam fuga cumque vel ea officiis
-        nesciunt non dolorem, blanditiis cum pariatur! Vel itaque eum eligendi
-        perspiciatis ratione labore, odio eos ipsa corrupti possimus veritatis
-        iste atque animi voluptate facilis a nemo molestias unde. Suscipit,
-        provident minus. Minus dolores fugiat, veniam modi quidem excepturi
-        quibusdam sequi doloribus! Quae tenetur quod suscipit itaque
-        necessitatibus, soluta architecto laudantium, repellat hic ratione unde
-        provident, inventore voluptate aliquam. Fugit culpa adipisci officiis
-        suscipit molestiae et consequatur error dolor, iure, velit nulla. Error
-        harum incidunt corrupti pariatur, sit ex blanditiis? Dicta magni dolor
-        tempora quod vel, illum necessitatibus, temporibus quaerat laboriosam
-        nisi quas corrupti. Iste voluptatum aspernatur reiciendis minima aperiam
-        maiores culpa delectus fuga sequi! Veniam molestias voluptatum quam
-        temporibus, quasi officia doloremque aut numquam commodi, quaerat velit
-        iusto. Enim, aliquam et ab ipsum, debitis animi cumque earum possimus
-        quos reprehenderit doloremque voluptas! Aliquid, voluptatem! Enim vero
-        unde error ratione voluptates perspiciatis odio fugit, ipsa pariatur
-        optio sequi qui nobis quisquam reprehenderit dolorem dolores,
-        repellendus quod! Omnis quasi eveniet laudantium exercitationem natus
-        numquam sint vel aliquid ea.
-      </ExpandableText>
- 
+    <div>
+      <Form />
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,19 @@
+const Form = () => {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="Age" className="form-label">Age</label>
+        <input id="Age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
