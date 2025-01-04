2aae0d5f4388950a075baca61d2e433640b52daa
Juhee Dhone <juheedhone12@gmail.com>
2025-01-05 00:18:22 +0530

    Effect Dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,20 @@
 import { useState } from "react";
-import ExpenseFilter from "./Expense-tracker/Component/ExpenseFilter";
-import ExpenseForm from "./Expense-tracker/Component/ExpenseForm";
-import ExpenseList from "./Expense-tracker/Component/ExpenseList";
+import ProductList from "./components/ProductList";
 
 const App = () => {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    {
-      id: 1,
-      description: "aa",
-      amount: 10,
-      category: "utilities",
-    },
-    {
-      id: 2,
-      description: "ba",
-      amount: 10,
-      category: "utilities",
-    },
-    {
-      id: 3,
-      description: "cc",
-      amount: 10,
-      category: "utilities",
-    },
-    {
-      id: 4,
-      description: "dd",
-      amount: 10,
-      category: "utilities",
-    },
-  ]);
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  const [category, setCategory] = useState("");
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(newExpense) =>
-            setExpenses([
-              ...expenses,
-              { ...newExpense, id: expenses.length + 1 },
-            ])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      ></ExpenseList>
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event?.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="household">household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 };
