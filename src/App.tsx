4b0fb54e1d7567cfcbb7e164f08e19266e3fd5f9
Juhee Dhone <juheedhone12@gmail.com>
2025-01-05 17:22:31 +0530

    Error Handling

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,25 @@ interface user {
 
 const App = () => {
   const [users, setUsers] = useState<user[]>([]);
+  const [error, setError] =useState('')
 
   useEffect(() => {
     axios
-      .get<user[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .get<user[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch(err=> setError(err.message));
     // .then((res) => console.log(res.data[0].name));
   }, []);
-  return <ul>
-    {users.map(user=> <li key={user.id}>{user.name}</li>)}
-  </ul>;
+  return (
+    <>
+    {error &&  <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default App;
