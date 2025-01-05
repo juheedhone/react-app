9971df86b60e4eee481b8488836fbf9570fe1e5d
Juhee Dhone <juheedhone12@gmail.com>
2025-01-05 22:15:02 +0530

    Deleting Data

--- src/App.tsx
+++ src/App.tsx
@@ -29,13 +29,36 @@ const App = () => {
 
     // .then((res) => console.log(res.data[0].name));
   }, []);
+
+  const deleteUser = (user: user) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}{" "}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
